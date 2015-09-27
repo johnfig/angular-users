f06ae37765b0da1b4b607b4163e67f39f8baf647
John Figueiredo <john@johnfigueiredo.com>
2015-09-27 14:22:17 -0700

    Add likes to angular application

--- app/assets/javascripts/controllers/users-controller.js
+++ app/assets/javascripts/controllers/users-controller.js
@@ -1,4 +1,19 @@
-app.controller('UserController', ['$scope', function($scope) {
+app.controller('UserController', ['$scope',
+function($scope) {
   $scope.name = 'John Fig';
   $scope.age  = '28';
+  $scope.likes = 0;
+  $scope.plusOne = function(index) {
+    $scope.likes += 1;
+  };
+  $scope.users = [
+                   {
+                      name: 'John Fig',
+                      age: 28
+                   },
+                   {
+                      name: 'Demetria',
+                      age: 27
+                   }
+                ];
 }]);
